442443c8454a737c84fd8fbee6c96f9b2eb017e2
BarkingTiger <102829934+BarkingTiger@users.noreply.github.com>
2022-04-19 18:21:24 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
+/*
+Kevin Khau
+Modded Rocket Patrol
+4/19/21
+~3-4 hours
+
+Implement a simulatneous two-player mode (30)
+P1 Controls:
+A = Move Left
+D = Move Right
+W = Fire
+P2 Controls:
+Left = Move Left
+Right = Move Right 
+Up = Fire
+Create a new spaceship type that's smaller , moves faster, and is worth more points (20)
+Create new artwork for all of the in-game assets (20)
+Display the time remaining on the screen(10)
+Implement parallax scrolling(10)
+Create a new scrolling tile sprite for the background (5)
+Implement the speed inreate that happens after 30 seconds in the original game (5)
+*/
 let config = {
     type: Phaser.CANVAS,
     width: 640, 
